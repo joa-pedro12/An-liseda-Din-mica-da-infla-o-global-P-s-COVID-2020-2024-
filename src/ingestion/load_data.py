06bbb0d607041d3ee466a902b65c488cb73f91bd
joa-pedro12 <joaopedrocabral92@gmail.com>
2026-05-06 16:00:13 -0300

    atualização na função load_data

--- src/ingestion/load_data.py
+++ src/ingestion/load_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pathlib import Path
 
 
-def load_data(file_name: str) -> pd.DataFrame:
+def load_data(file_name: str = "raw/global_inflation_post_covid.csv") -> pd.DataFrame:
     try:
         base_path = Path(__file__).resolve().parents[2]
         data_path = base_path / "dados_Covid" / file_name
